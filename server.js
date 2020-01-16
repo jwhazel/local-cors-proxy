39c21dd7b5b0f02b7b276f3e74ad0942dc253e2e
Jesse Hazel <jwhazel@me.com>
2020-01-16 12:28:41 -0500

    Fix query param

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const cors = require("cors");
 const axios = require("axios");
 const port = 1234;
 
+//Enable cors
 app.use(cors());
 
 app.all("/", function(req, res, next) {
   //Accept 2 query params: q and json
-  let query = req.query.q || "";
+  let q = req.query.q || "";
   let json = req.query.json || "false";
 
   //Coerce to JSON header if it looks like the request is JSON
   let isJSON =
-    query.toLowerCase().includes("json") || json.toLowerCase() == "true";
+    q.toLowerCase().includes("json") || json.toLowerCase() == "true";
 
-  //Request the resource and send it (cors package takes care of setting cors header)
-  axios(req.query.q).then(data => {
+  //Request the resource and send the response back
+  axios(q).then(data => {
     if (isJSON) {
       res.json(data.data);
     } else {
