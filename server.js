9deaef23000095145eb57ad3fbce887281b0fea7
Jesse Hazel <jwhazel@me.com>
2022-11-04 20:29:31 -0400

    feat: allow headers to pass through

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 const app = require("express")();
 const cors = require("cors");
 const axios = require("axios");
+const https = require("https");
 const port = 1234;
 
 //Enable cors
 app.use(cors());
 
-app.all("/", function(req, res, next) {
+app.all("/", function (req, res, next) {
   //Accept 2 query params: q and json
   let q = req.query.q || "";
   let json = req.query.json || "false";
 
   //Coerce to JSON header if it looks like the request is JSON
-  let isJSON =
-    q.toLowerCase().includes("json") || json.toLowerCase() == "true";
+  let isJSON = q.toLowerCase().includes("json") || json.toLowerCase() == "true";
+
+  // Fix an issue with localhost and certs
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+  });
 
   //Request the resource and send the response back
-  axios(q).then(data => {
-    if (isJSON) {
-      res.json(data.data);
-    } else {
-      res.send(data.data);
-    }
-  });
+  axios(q, {
+    method: req.method,
+    headers: req.headers,
+    httpsAgent: agent,
+  })
+    .then((data) => {
+      if (isJSON) {
+        res.json(data.data);
+      } else {
+        res.send(data.data);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).send(e.message);
+    });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`CORS-enabled web server listening on port ${port}`);
 });
